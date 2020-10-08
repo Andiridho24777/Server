c856981df4c6a7e3dadfd11924c728c2b6e956b5
Andiridho24777 <70737171+Andiridho24777@users.noreply.github.com>
2020-10-08 12:46:30 +0800

    Update Farzhadcore.lua

--- Farzhadcore.lua
+++ Farzhadcore.lua
@@ -1,4 +1,168 @@
-  os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/cache/GCloud.ini")
+function main()
+  Amenu = gg.choice({
+  	"🔰Protect Antiban",
+      "🏡One Click Lobby",
+      "🎮One Click Game",
+      "🔚Exit",
+}, nil, (os.date("Farzhad | Simpel Fast Script\n%d %B %Y %H:%M%p")))
+if Amenu ==  1 then
+A()
+end
+if Amenu == 2 then
+B()
+end
+if Amenu == 3 then
+C()
+end
+if Amenu == 4 then
+D()
+end
+Pharao = -1
+end
+
+function A()
+gg.clearResults()
+    gg.setRanges(4)
+    gg.searchNumber('67109633', 4)
+    gg.getResults(999999)
+    gg.editAll('0', 4)
+    gg.clearResults()
+    gg.clearResults()
+    gg.setRanges(4)
+    gg.searchNumber('67109377', 4)
+    gg.getResults(999999)
+    gg.editAll('0', 4)
+    gg.clearResults()
+    gg.clearResults()
+    gg.setRanges(4)
+    gg.searchNumber('12547', 4)
+    gg.getResults(999999)
+    gg.editAll('0', 4)
+    gg.clearResults()
+    gg.clearResults()
+    gg.setRanges(4)
+    gg.searchNumber('131586', 4)
+    gg.getResults(999999)
+    gg.editAll('0', 4)
+    gg.clearResults()
+    gg.clearResults()
+    gg.setRanges(4)
+    gg.searchNumber('67109633', 4)
+    gg.getResults(999999)
+    gg.editAll('0', 4)
+    gg.clearResults()
+    gg.clearResults()
+    gg.setRanges(4)
+    gg.searchNumber('67109377', 4)
+    gg.getResults(999999)
+    gg.editAll('0', 4)
+    gg.clearResults()
+    gg.clearResults()
+    gg.setRanges(4)
+    gg.searchNumber('12547', 4)
+    gg.getResults(999999)
+    gg.editAll('0', 4)
+    gg.clearResults()
+    gg.clearResults()
+    gg.setRanges(4)
+    gg.searchNumber('131586', 4)
+    gg.getResults(999999)
+    gg.editAll('0', 4)
+    gg.clearResults()
+end
+
+function B()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1,348,759,109;1953067887;1,634,692,166;1,920,287,604::28", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1634692166", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10)
+gg.editAll("1634692266", gg.TYPE_DWORD)
+gg.toast("New Les Recoil Aktive")
+gg.clearResults()
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_DATA)
+  gg.setRanges(gg.REGION_C_DATA)
+  gg.searchNumber("360;0.0001;1478828288", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("0.0001", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(100)
+  gg.editAll("9999", gg.TYPE_FLOAT)
+  gg.toast("Aimlock 100M Aktive")
+  gg.clearResults()
+gg.setRanges(8) 
+gg.searchNumber("-1,901,891,198,734,303,227", 32, false, 536870912, 0, -1) 
+gg.getResultsCount() 
+gg.getResults(1) 
+gg.editAll("-1,901,891,198,902,075,392", 32) 
+gg.clearResults() 
+end
+    
+    
+   
+
+  
+    
+    function C()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;1;1;0.0001;20;0.0005;0.4::50", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(300)
+gg.editAll("1.07", gg.TYPE_FLOAT)
+gg.toast("micro speed")
+gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("736370688D;8D;937041920D;32D;904011776D:17", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("32", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+    gg.getResults(100)
+    gg.editAll("30", gg.TYPE_DWORD)
+    gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber('"1.1754944e-38F;3.0F:197"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber('"3"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("6969", gg.TYPE_FLOAT)
+gg.refineNumber('"3"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+revert = gg.getResults(2, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(2, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_FLOAT then
+		v.value = "3"
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber('"1.1754944e-38F;3.0F:197"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber('"3"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("6969", gg.TYPE_FLOAT)
+gg.refineNumber('"3"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+revert = gg.getResults(3, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(3, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_FLOAT then
+		v.value = "140"
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber('"6969"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1000)
+gg.editAll("3", gg.TYPE_FLOAT)
+gg.clearResults()
+os.exit()
+end
+    
+
+function D()
+os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/cache/GCloud.ini")
   os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/GameErrorNoRecords")
   os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/tbslog")
   os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/ca-bundle.pem")
@@ -66,97 +230,7 @@ os.remove("/stroage/emulated/Android/data/com.tencent.iglite/cache/")
 os.remove("/storage/emulated/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs")
 os.remove("/storage/emulated/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo")
 os.remove("/storage/emulated/Android/data/com.tencent.iglite/files/ca-bundle.pem")
-
-  function main()
-  Amenu = gg.choice({
-    "🏠One Click Lobby",
-    "🎮Ond Click Game",
-    "🙎‍♂️Colour",
-    "🔙Keluar",
-}, nil, (os.date("Farzhad | Simpel Fast Cheat\n%d %B %Y %H:%M%p")))
-  if Amenu == 1 then
-    A()
-  end
-  if Amenu == 2 then
-    B()
-  end
-  if Amenu == 3 then
-     C()
-     end
-  if Amenu == 4 then
-     D()
-     end
-  Pharao = -1
-end
-
-function A()
-gg.setRanges(gg.REGION_C_DATA) 
-gg.searchNumber("360;0.0001;1478828288", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1) 
-gg.refineNumber("0.0001", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1) 
-gg.getResults(100) 
-gg.editAll("99999999", gg.TYPE_FLOAT) 
-gg.searchNumber("360;0.0001;1478828288", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1) 
-gg.refineNumber("0.0001", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1) 
-gg.getResults(100) 
-gg.editAll("99999999", gg.TYPE_FLOAT) 
-gg.clearResults() 
-gg.setRanges(gg.REGION_C_DATA) 
-gg.searchNumber("3.5;1;0.5;200;20::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1) 
-gg.searchNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1) 
-gg.getResults(100) 
-gg.editAll("99999999", gg.TYPE_FLOAT) 
-gg.clearResults() 
-gg.toast("Aimbot") 
-gg.clearResults()
-gg.setRanges(gg.REGION_ANONYMOUS)
-gg.searchNumber("1.5584387e28", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
-gg.getResults(100)
-gg.editAll("0", gg.TYPE_FLOAT)
-gg.clearResults()
-gg.setRanges(gg.REGION_ANONYMOUS)
-gg.searchNumber("1D;0.05000000075F;0.10000000149F;0.55000001192F;9.5F;15.0F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
-gg.searchNumber("1", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.getResults(800)
-gg.editAll("0", gg.TYPE_DWORD)
-gg.toast("Less Recoil")
-end
-
-function B()
-gg.setRanges(32)
-gg.searchNumber("88.50576019287F;87.27782440186F;-100.91194152832F;1F::13", 16, false, 536870912, 0, -1)
-gg.searchNumber("88.50576019287F;87.27782440186F;1F", 16, false, 536870912, 0, -1)
-gg.getResults(6)
-gg.editAll("1.96875;1.96875;999;1.96875;1.96875;999", 16)
-gg.clearResults()
-gg.setRanges(gg.REGION_ANONYMOUS)
-gg.processResume()
-gg.searchNumber('"9.201618;30.5;25"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
-gg.processResume()
-gg.refineNumber('"30.5"', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
-revert = gg.getResults(2000, nil, nil, nil, nil, nil, nil, nil, nil)
-gg.editAll('"230"', gg.TYPE_FLOAT)
-gg.processResume()
-gg.toast("Magic bullet")
-end
-
-function C()
-gg.setRanges(gg.REGION_ANONYMOUS)
-gg.searchNumber('1,120,403,456;0;1,220,861,952::9', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.searchNumber('1,120,403,456', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.getResults(3)
-gg.editAll('-1,027,211,264', gg.TYPE_DWORD)
-gg.clearResults()
-gg.setRanges(gg.REGION_C_ALLOC)
-gg.searchNumber("3D;25D;8D;40D;8D:25", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
-gg.refineNumber("40", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
-gg.getResults(2)
-gg.editAll("2", gg.TYPE_DWORD)
-gg.toast("Colour")
-gg.clearResults()
-end
-
-function D()
-  gg.toast("Keluar")
+gg.toast("Exit")
   os.exit()
 end
 
@@ -170,3 +244,4 @@ while true do
     main()
    end
  end
+ 
